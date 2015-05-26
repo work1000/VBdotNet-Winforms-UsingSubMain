0fd4a493a0098b83ce912b0d2071f6598a35eaa2
creative_software_gun <creativesoftwaregun@gmail.com>
2015-05-26 23:27:12 +0800

    .

---
+++ WindowsApplication1/Class1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Forms
+
+Public Class Class1
+
+    Public Shared Sub Main()
+
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1)
+
+    End Sub
+End Class
